0fae1de7ba40d1053529d025faac0131dc7ed57c
Johny Mattsson <johny.mattsson+github.gmail.com>
2011-11-27 17:51:02 +1100

    Stop messing with the compiler's idea of register offsets.

--- tos/chips/atm328p/atm328phardware.h
+++ tos/chips/atm328p/atm328phardware.h
@@ -1,14 +1,13 @@
 #ifndef _ATM328PHARDWARE_H_
 #define _ATM328PHARDWARE_H_
 
-#define __SFR_OFFSET 0x00
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
 typedef uint8_t __nesc_atomic_t;
 
 inline void __nesc_enable_interrupt() {
-     sei ();
+    sei ();
 }
 
 inline void __nesc_disable_interrupt() {
@@ -31,12 +30,12 @@ inline void __nesc_atomic_end (__nesc_atomic_t old_SREG)
 }
 
 #define SFR_BIT_SET(reg, bit) \
-    asm ("sbi %0, %1" : : "I" (reg), "I" (bit) )
+    asm ("sbi %0, %1" : : "I" (reg - __SFR_OFFSET), "I" (bit) )
 
 #define SFR_BIT_CLR(reg, bit) \
-    asm ("cbi %0, %1" : : "I" (reg), "I" (bit) )
+    asm ("cbi %0, %1" : : "I" (reg - __SFR_OFFSET), "I" (bit) )
 
 #define SFR_BIT_READ(reg, bit) \
-    ((*((uint8_t *)reg) & _BV(bit)) != 0)
+    ((*((uint8_t *)(reg - __SFR_OFFSET)) & _BV(bit)) != 0)
 
 #endif
